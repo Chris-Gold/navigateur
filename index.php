32d0e8117c39ba01a3e74583a3cd5ef4a6999541
LyraKuroneko <indigo.laetitia@gmail.com>
2018-01-26 11:41:11 +0100

    update

--- index.php
+++ index.php
@@ -3,9 +3,23 @@
         <head>
             <?php include 'includes/header.html' ?>
             <title>Nyavigateur</title>
-
+            <script src="js/javascript.js"></script>
         </head>
         <body>
+            <div class="div1 container jumbotron text-center">
+                <?php $list = scandir('/var/www/navigateur'); 
+                $max = sizeof($list);
+                //              
+                print_r($list[3]);
+                for ($i=0; $i < $max; $i++) { 
+                    if(strpos($list[$i],".") == true){
+                        print $list[$i] . "</br>";
+                    }
+                    else{
+                        print "<a href=" .$list[$i].">" .$list[$i]. "</a>"."</br>";
+                    }
+                }?>
+            </div>
             <?php include 'includes/base_js.html' ?>
         </body>
     </html>
---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    /*$(".div1").load("../navigateur/", function(responseTxt, statusTxt, xhr){
+        if(statusTxt == "success")
+            alert("External content loaded successfully!");
+        if(statusTxt == "error")
+            alert("Error: " + xhr.status + ": " + xhr.statusText);
+    });
+
+    $('.div1').fileTree({
+        root: '/some/folder/',
+        script: 'jqueryFileTree.asp',
+        expandSpeed: 1000,
+        collapseSpeed: 1000,
+        multiFolder: false
+    }, function(file) {
+        alert(file);
+    });*/
+
+    $(".div1")
+});
